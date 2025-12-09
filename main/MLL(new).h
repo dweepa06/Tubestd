0ec5430b848ee92b0f646fdcfb78ffd6fbb66510
dweepa06 <aryodwipa203@gmail.com>
2025-12-09 16:07:04 +0700

    Create MLL(new).h

---
+++ main/MLL(new).h
@@ -0,0 +1,60 @@
+#ifndef MLL_H_INCLUDED
+#define MLL_H_INCLUDED
+#include<iostream>
+using namespace std;
+
+typedef struct elmAkun *adrAkun;
+typedef struct elmStatus *adrStatus;
+
+struct akun{
+    string nama;
+    string bio;
+    string email;
+    string status;
+
+};
+struct listAkun{
+    adrAkun first;
+
+};
+
+struct elmAkun{
+    akun info;
+    adrAkun next;
+    adrStatus firstStatus;
+};
+
+struct status{
+    string
+};
+
+struct elmStatus{
+    status info;
+    adrStatus next;
+    adrStatus prev;
+};
+
+
+void menu();
+void createListAkun(listAkun &L);
+bool isEmptyAkun(listAkun L);
+bool isEmptyStatus(adrAkun p);
+adrAkun createElemenAkun(string namaAkun,string bio,string email);
+adrStatus createElemenStatus(string status);
+void addAkun(listAkun &L,adrAkun p);
+void addStatus(adrAkun p,adrStatus q);
+adrAkun searchAkunByNama(listAkun L,string namaAkun);
+adrAkun searchAkunByEmail(listAkun L,string email);
+//searchAkunByStatus
+void deleteBio(adrAkun p);
+//delete status
+int countAkun(listAkun L);
+int viewHuruf(adrAkun q);
+
+
+
+
+
+#endif // MLL_H_INCLUDED
+
+#endif // MLL_H_INCLUDED
